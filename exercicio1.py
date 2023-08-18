abf4f33400b5da5091863a6a501bd962e5da3e2e
antoniobfm <toinho111@gmail.com>
2023-08-18 02:17:58 -0300

    Solved both exercises

--- exercicio1.py
+++ exercicio1.py
@@ -1,26 +1,11 @@
 #  Se achar necessario, faça import de outras bibliotecas
 
-
-
-
-
 # Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
+def conta_palavras_unicas(texto):
+    # Remove pontuações, transforma tudo em letras minúsculas e depois separa as palavras
+    palavras = ''.join([c if c.isalnum() or c.isspace() else ' ' for c in texto]).lower().split()
+    
+    # Usa um conjunto (set) para armazenar palavras únicas e retorna seu tamanho
+    return len(set(palavras))
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
--- exercicio2.py
+++ exercicio2.py
@@ -1,26 +1,12 @@
 #  Se achar necessario, faça import de outras bibliotecas
 
-
-
-
-
 # Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
+def is_anagram(string1, string2):
+    # Remove espaços e transforma tudo em letras minúsculas
+    string1 = string1.replace(" ", "").lower()
+    string2 = string2.replace(" ", "").lower()
+    
+    # Retorna True se as strings ordenadas forem iguais
+    return sorted(string1) == sorted(string2)
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
